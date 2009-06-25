c2a0d40fffec7eeef760c8eafc84115eba829066
Alex Gaynor <alex.gaynor@gmail.com>
2009-06-24 21:07:34 -0500

    umm, fix some fail

--- django_object_cache/manager.py
+++ django_object_cache/manager.py
@@ -3,7 +3,7 @@ from django.db.models import Manager
 from django_object_cache.query import CacheQuerySet
 
 class CacheManager(Manager):
-    use_for_related_objects = True
+    use_for_related_fields = True
 
     def get_query_set(self):
         return CacheQuerySet(self.model)
